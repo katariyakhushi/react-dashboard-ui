a99ef2fd99fc1c75057da0c16cd3e786e1a78d07
khushikatariya <katariyakhushi373@gmail.com>
2023-09-26 19:41:07 +0530

    create table

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TableData from "./components/TableData";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <TableData/>
+    </div>
+  );
+}
+
+export default App;
