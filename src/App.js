41558f3c696eb458be1b799e28179eeae96e63da
khushikatariya <katariyakhushi373@gmail.com>
2023-10-07 20:28:29 +0530

    add signup page

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import CompanyInfo from "./components/CompanyInfo";
 import TableData from "./components/TableData"; 
 import MyFooter from "./components/MyFooter";
 import Signup from "./components/Signup"
+import paggination from './components/paggination.1';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/company" element={<CompanyInfo />} />
       <Route path="/" element={<TableData />} /> 
+      <Route path="/paggination" element={<paggination />} /> 
       <Route path="/MyFooter" element={<MyFooter />} />
       <Route path="/Signup" element={<Signup />} />
     </Routes>
