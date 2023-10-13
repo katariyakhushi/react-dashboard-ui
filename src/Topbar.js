2d8d302ea3a3eaf825a537b1e9c14f271bcdd2dc
khushikatariya <katariyakhushi373@gmail.com>
2023-10-13 21:42:13 +0530

    more updates

--- src/Topbar.js
+++ src/Topbar.js
@@ -12,16 +12,17 @@ const Topbar = () => {
       </div>
       <div className="right-section">
       <div  >
+      
       <select className="dropdown">
-        <option value=""> option</option>
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
+        <option value="" className="option"> option</option>
+        <option value="option1" className="option">Option 1</option>
+        <option value="option2"  className="option">Option 2</option>
+        <option value="option3"  className="option">Option 3</option>
       </select>
      </div>
         <div className="sign-up-button">
         <Link to="/Signup" > 
-          <button style={{ backgroundColor: 'orange', color: 'white' }}>Signup</button>
+          <button style={{ backgroundColor: 'orange', color: 'white', }}>Signup</button>
         </Link>
         </div>
       
@@ -29,9 +30,10 @@ const Topbar = () => {
       
     </div>
     <div className="information">
-       <h1>Find & Research Information Technology and Services Companies in Ivory Coast</h1>
-    <h6>Get access to 6sense's database for more than 235 Information Technology and Services companies in Ivory Coast.</h6>
+       <h1 style={{marginLeft :'60px', fontSize:'50px'}}><b>Find & Research Information Technology and Services Companies in Ivory Coast</b></h1>
+    <h6 style={{marginLeft :'60px',fontSize:'20px'}}>Get access to 6sense's database for more than 235 Information Technology and Services companies in Ivory Coast.</h6>
     </div>
+   
    
     </div>
    
--- src/components/TableData.js
+++ src/components/TableData.js
@@ -33,20 +33,29 @@ function TableData() {
 
   return (
       <div>
-        <div> <input type="text"  placeholder="Search for companies...."  className="search-bar"/>
+        
+        <div className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search" style={{marginLeft:'60px',marginTop:'40px',marginBottom:'40px'}}>   <input
+          type="search"
+          className="form-control form-control-dark text-bg-dark"
+          placeholder="Search..."
+          aria-label="Search"
+          size={70}
+          style={{ height: '40px', borderRadius:'8px', marginLeft:'20px'}}
+        />
+        
         </div>
        
       <div className="table-container">
          
        <TableContainer component={Paper}>
         <Table>
-          <TableHead>
-            <TableCell>Avtar</TableCell>
-            <TableCell>Company Name</TableCell>
-            <TableCell>Employees</TableCell>
-            <TableCell>Industry</TableCell>
-            <TableCell>Website</TableCell>
-            <TableCell>More</TableCell>
+          <TableHead >
+            <TableCell style={{ fontWeight: 'bold',fontSize: '150%'}}>Avtar</TableCell>
+            <TableCell style={{ fontWeight: 'bold', fontSize: '150%'}}>Company Name</TableCell>
+            <TableCell style={{ fontWeight: 'bold', fontSize: '150%'}}>Employees</TableCell>
+            <TableCell style={{ fontWeight: 'bold', fontSize: '150%'}}> Industry</TableCell>
+            <TableCell style={{ fontWeight: 'bold', fontSize: '150%'}}>Website</TableCell>
+            <TableCell style={{ fontWeight: 'bold', fontSize: '150%',alignItems:'center', }}>More</TableCell>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
@@ -71,7 +80,7 @@ function TableData() {
                         protein: row.protein,
                       });
                     }}
-                    style={{ fontSize: '14px', height: '25px', width: '80px', margin: '10px' }}
+                    style={{ fontSize: '15px', height: '35px', width: '60%', marginRight:'100%'}}
                   >
                     Company
                   </Button>
@@ -82,9 +91,23 @@ function TableData() {
         </Table>
       
       </TableContainer>
+      
+      
+      
+    </div>
+     
+    <div className="myComponentContainer text-center">
+      <h1 className="border border-dark rounded p-3 myComponentTitle">
+        Get instant access to B2B contact data across the web
+        <div className="d-flex justify-content-center">
+        <button className="btn btn-dark myComponentButton  ">Sign Up Today</button>
+      </div>
+      </h1>
+   
+    </div>
     </div>
   
-  </div>
+  
   );
 }
 
